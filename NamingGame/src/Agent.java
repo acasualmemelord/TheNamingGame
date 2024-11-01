56d99f57d38987f192d4897bcca0526b85eeba8f
acasualmemelord <62784122+acasualmemelord@users.noreply.github.com>
2024-10-31 19:21:05 -0500

    yeah

---
+++ NamingGame/src/Agent.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+
+public class Agent {
+	private int id;
+	private String startingWord;
+	private ArrayList<String> words;
+	
+	public Agent (int id, String startingWord) {
+		this.id = id;
+		this.startingWord = startingWord;
+		this.words = new ArrayList<String>();
+		words.add(startingWord);
+	}
+
+	public String getStartingWord() {
+		return startingWord;
+	}
+	
+	public String getTopWord() {
+		return words.get(0);
+	}
+	
+	public String getRandomWord() {
+		Random r = new Random();
+		return words.get(r.nextInt(0, words.size()));
+	}
+	
+	public int inventory() {
+		return words.size();
+	}
+	
+	public boolean contains(String word) {
+		return words.contains(word);
+	}
+	
+	public void addWord(String word) {
+		words.add(word);
+	}
+	
+	public void removeAllExcept(String word) {
+		for (Iterator<String> iterator = words.iterator(); iterator.hasNext(); ) {
+		    String str = iterator.next();
+		    if (!str.equals(word)) {
+		        iterator.remove();
+		    }
+		}
+	}
+	
+	public String toString() {
+		return String.format("Agent %d (starting word %s): %s", id, startingWord, Arrays.toString(words.toArray()));
+	}
+}
---
+++ NamingGame/src/Main.java
@@ -0,0 +1,65 @@
+import java.util.Random;
+
+public class Main {
+	public static void main(String[] args) {
+		Random r = new Random();
+		int steps = 0;
+		int agentNum = 20;
+		Agent[] agents = new Agent[agentNum];
+		for(int i = 0; i < agents.length; i ++) {
+			String startingWord = randomWord(r);
+			agents[i] = new Agent(i + 1, startingWord);
+		}
+		while(steps < 10000) {
+			System.out.print("step " + steps + ": ");
+			if(converged(agents)) {
+				System.out.println("all agents have converged");
+				break;
+			}
+			
+			int test = r.nextInt(0, agents.length);
+			int test2;
+			if (test == 0) test2 = 1;
+			else if (test == agents.length - 1) test2 = agents.length - 2;
+			else {
+				if (r.nextBoolean()) test2 = test - 1;
+				else test2 = test + 1;
+			}
+			Agent agent = agents[test];
+			Agent agent2 = agents[test2];
+			
+			String str = agent.getRandomWord();
+			System.out.println("Agent " + (test + 1) + " conveys word " + str);
+			if(agent2.contains(str)) {
+				System.out.println("Agent " + (test2 + 1) + " has that word");
+				agent2.removeAllExcept(str);
+			} else {
+				System.out.println("Agent " + (test2 + 1) + " does not have that word");
+				agent2.addWord(str);
+			}
+			System.out.println();
+			steps ++;
+		}
+		
+		for(Agent agent : agents) {
+			System.out.println(agent);
+		}
+	}
+
+	public static String randomWord(Random r) {
+		int length = r.nextInt(1, 11);
+		String result = "";
+		for(int i = 0; i < length; i ++) {
+			result += (char) (64 + r.nextInt(1, 26));
+		}
+		return result;
+	}
+	
+	public static boolean converged(Agent[] agents) {
+		String s = agents[0].getTopWord();
+		for(Agent agent : agents) {
+			if(agent.inventory() != 1 || !agent.getTopWord().equals(s)) return false;
+		}
+		return true;
+	}
+}
